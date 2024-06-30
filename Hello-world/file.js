b0faed7f9a518acb6b5197c3f0fac8d610c83e4b
PreritNag <preritnag4@gmail.com>
2024-06-30 14:07:58 +0530

    node js

--- Hello-world/file.js
+++ Hello-world/file.js
@@ -1,8 +1,17 @@
+const { log } = require("console");
 const fs=require("fs");
-// Sync
+Sync
  fs.writeFileSync("file1.txt","Hello World");
 
- //Async
+ Async
  fs.writeFile("file2.txt","hello my name is prerit",(err)=>{});
 
- 
+ const result=fs.readFileSync("./contact.txt","utf-8");
+ console.log(result);
+
+fs.readFile("./contact.txt", "utf-8",(err,result)=>{
+    if(err){console.log("error",err)}
+    else{console.log("result",result)}
+});
+
+fs.appendFileSync("./file1.txt",`hello brother`)
